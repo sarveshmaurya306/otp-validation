ee75b5c39e37cee465cb9ecbe027968ca6f03f86
sarvesh kumar <sarveshmaurya306@gmail.com>
2023-09-23 14:03:56 +0530

    moved client into server for deployment

--- client/src/api/otpApi.js
+++
@@ -1,8 +0,0 @@
-import instance from './config/axios';
-
-const otpApi= {
-    getOtp: (data)=> instance.post('/get-otp', data),
-    verifyOtp: (data)=> instance.post('/verify-otp', data)
-}
-
-export default otpApi;
